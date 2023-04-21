8c99b9ac0501d8a92e44d6a54971d542561696dc
MariaA-109 <122259443+MariaA-109@users.noreply.github.com>
2023-04-21 18:01:59 +0200

    added css styles

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -1,65 +1,70 @@
 export default function Header() {
   return (
     <>
-      <div className="head">
-        <a href="https://news.ycombinator.com/news">
-          <img
-            src="https://news.ycombinator.com/y18.gif"
-            alt="logo-img"
-            className="logo"
-          ></img>
-        </a>
-        <h4>Hacker News</h4>
-        <li>
-          <a className="links" href="https://news.ycombinator.com/newest">
-            new
+      <div className="header-container">
+        <div className="head">
+          <a href="https://news.ycombinator.com/news">
+            <img
+              src="https://news.ycombinator.com/y18.gif"
+              alt="logo-img"
+              className="logo"
+            ></img>
           </a>
-        </li>
-        <p>|</p>
-        <li>
-          <a className="links" href="https://news.ycombinator.com/front">
-            past
-          </a>
-        </li>
-        <p>|</p>
-        <li>
-          <a className="links" href="https://news.ycombinator.com/newcomments">
-            comments
-          </a>
-        </li>
-        <p>|</p>
-        <li>
-          <a className="links" href="https://news.ycombinator.com/ask">
-            ask
-          </a>
-        </li>
-        <p>|</p>
-        <li>
-          <a className="links" href="https://news.ycombinator.com/show">
-            show
-          </a>
-        </li>
-        <p>|</p>
-        <li>
-          <a className="links" href="https://news.ycombinator.com/jobs">
-            jobs
-          </a>
-        </li>
-        <p>|</p>
-        <li>
-          <a className="links" href="https://news.ycombinator.com/submit">
-            submit
-          </a>
-        </li>
-        <p>|</p>
-        <li className="login">
-          <a
-            className="links"
-            href="https://news.ycombinator.com/login?goto=news"
-          >
-            log in
-          </a>
-        </li>
+          <h4>Hacker News</h4>
+          <li>
+            <a className="links" href="https://news.ycombinator.com/newest">
+              new
+            </a>
+          </li>
+          <p>|</p>
+          <li>
+            <a className="links" href="https://news.ycombinator.com/front">
+              past
+            </a>
+          </li>
+          <p>|</p>
+          <li>
+            <a
+              className="links"
+              href="https://news.ycombinator.com/newcomments"
+            >
+              comments
+            </a>
+          </li>
+          <p>|</p>
+          <li>
+            <a className="links" href="https://news.ycombinator.com/ask">
+              ask
+            </a>
+          </li>
+          <p>|</p>
+          <li>
+            <a className="links" href="https://news.ycombinator.com/show">
+              show
+            </a>
+          </li>
+          <p>|</p>
+          <li>
+            <a className="links" href="https://news.ycombinator.com/jobs">
+              jobs
+            </a>
+          </li>
+          <p>|</p>
+          <li>
+            <a className="links" href="https://news.ycombinator.com/submit">
+              submit
+            </a>
+          </li>
+          <p>|</p>
+          <li className="login">
+            <a
+              className="links"
+              href="https://news.ycombinator.com/login?goto=news"
+            >
+              log in
+            </a>
+          </li>
+        </div>
       </div>
     </>
   );
--- src/Components/NewsSnippet.js
+++ src/Components/NewsSnippet.js
@@ -1,21 +1,46 @@
 export default function NewsSnippet({ result }) {
-    return(
+  return (
     <li>
-        <div className="news-container">
-            <a className="title" href={result.url}>
-                {result.title}
-            </a>
-            <a href={result.url}>{result.url}</a>
+      <div>
+        <div>
+          <a className="title" href={result.url}>
+            {result.title}
+          </a>
+          <a className="url" href={result.url}>
+            ({result.url})
+          </a>
         </div>
-        <div className="news-container">
-            <p>{result.points} points by 
-            <a href={`https://news.ycombinator.com/user?id=${result.author}`}>{result.author}</a>
-            <a href={`https://news.ycombinator.com/item?id=${result.objectID}`} className="tooltip"> x hours ago <div className="tooltiptext">{result.created_at}</div></a>
-            <a href={`https://news.ycombinator.com/hide?id=${result.objectID}&goto=news`}>hide</a>
-            <a href={`https://news.ycombinator.com/hide?id=${result.objectID}`}>{result.num_comments} comments</a>
-
-            </p>
-        </div>
+      </div>
+      <div>
+        <p className="link">
+          {result.points} points by
+          <a
+            className="link"
+            href={`https://news.ycombinator.com/user?id=${result.author}`}
+          >
+            {result.author}
+          </a>
+          <a
+            href={`https://news.ycombinator.com/item?id=${result.objectID}`}
+            className="tooltip"
+          >
+            {" "}
+            x hours ago <div className="tooltiptext">{result.created_at}</div>
+          </a>
+          <a
+            className="link"
+            href={`https://news.ycombinator.com/hide?id=${result.objectID}&goto=news`}
+          >
+            | hide |
+          </a>
+          <a
+            className="link"
+            href={`https://news.ycombinator.com/hide?id=${result.objectID}`}
+          >
+            {result.num_comments} comments
+          </a>
+        </p>
+      </div>
     </li>
-    )
-};
+  );
+}
